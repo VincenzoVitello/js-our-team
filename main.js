7dd52d3c7d9d473cd2ea0606c6a446c5a8914ee0
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2022-01-11 09:34:58 +0100

    molto codice è stato trasformato in funzioni

--- main.js
+++ main.js
@@ -3,52 +3,77 @@ const team = [
   {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    image: 'wayne-barnett-founder-ceo.jpg',
+    image: 'img/wayne-barnett-founder-ceo.jpg',
   },
   {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg',
+    image: 'img/angela-caroll-chief-editor.jpg',
   },
   {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg',
+    image: 'img/walter-gordon-office-manager.jpg',
   },
   {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg',
+    image: 'img/angela-lopez-social-media-manager.jpg',
   },
   {
     name: 'Scott Estrada',
     role: 'Developer',
-    image: 'scott-estrada-developer.jpg',
+    image: 'img/scott-estrada-developer.jpg',
   },
   {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    image: 'barbara-ramos-graphic-designer.jpg',
+    image: 'img/barbara-ramos-graphic-designer.jpg',
   },
 ];
 
 //dato questo array, devo ora appendere in html ogni oggetto
 //creo ciclo for che inserisca in html degli elementi con all'interno gli oggetti
-for (let index = 1; index < team.length; index++) {
-  let newCard = `<div class="team-card">
-                <div class="card-image">
-                  <img
-                    src="img/${team[index].image}"
-                    alt=""
-                  />
-                </div>
-                <div class="card-text">
-                  <h3>${team[index].name}</h3>
-                  <p>${team[index].role}</p>
-                </div>
-              </div>`;
-              document.getElementById('team-container').innerHTML += newCard
+function createDomCard( cardObject )
+{
+  document.getElementById('team-container').innerHTML += 
+                `<div class="team-card">
+                    <div class="card-image">
+                      <img
+                        src="${cardObject.image}"
+                        alt=""
+                      />
+                    </div>
+                    <div class="card-text">
+                      <h3>${cardObject.name}</h3>
+                      <p>${cardObject.role}</p>
+                    </div>
+                </div>`;
+ 
+} 
 
+for (let index = 1; index < team.length; index++) {
+  
+  createDomCard( team[index] )
 }
+
+
+//ora devo fare in modo che al click sul pulsante Add si possano aggiungere nuove cards
+
+const addBtn = document.getElementById('addMemberButton');
+addBtn.addEventListener('click', getCardData)
+
+function getCardData (){
+
+  let newMemberCard = {};
+  newMemberCard.name = document.getElementById('name').value;
+  newMemberCard.role = document.getElementById('role').value;
+  newMemberCard.image = document.getElementById('image').value;
+
+  createDomCard( newMemberCard );
+     
+  
+}
+
 
 
